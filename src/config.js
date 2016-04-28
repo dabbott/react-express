6f3fd456b49b5a67d2b0172d3fa7010b75ebb6c2
Devin Abbott <devinabbott@gmail.com>
2016-04-27 17:44:31 -0700

    Update open graph

--- src/config.js
+++ src/config.js
@@ -16,17 +16,19 @@ module.exports = Object.assign({
   apiPort: process.env.APIPORT,
   app: {
     title: 'React Native Express',
-    description: 'The fastest way to learn React Native.',
+    description: 'Learn React Native with interactive examples.',
     head: {
       titleTemplate: 'React Native Express: %s',
       meta: [
-        {name: 'description', content: 'The fastest way to learn React Native.'},
+        {name: 'description', content: 'Learn React Native with interactive examples.'},
         {charset: 'utf-8'},
+        {property: 'og:type', content: 'article'},
+        {property: 'og:url', content: 'http://www.reactnativeexpress.com/'},
         {property: 'og:site_name', content: 'React Native Express'},
         {property: 'og:image', content: 'https://www.reactnativeexpress.com/logo.png'},
         {property: 'og:locale', content: 'en_US'},
         {property: 'og:title', content: 'React Native Express'},
-        {property: 'og:description', content: 'The fastest way to learn React Native.'},
+        {property: 'og:description', content: 'Learn React Native with interactive examples.'},
         {property: 'og:card', content: 'summary'},
         {property: 'og:site', content: '@devinaabbott'},
         {property: 'og:creator', content: '@devinaabbott'},
