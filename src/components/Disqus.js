f4016ff99a86ed2cac3ad954b808aeaf214f37f7
Devin Abbott <devinabbott@gmail.com>
2016-11-27 12:20:22 -0800

    Prod / staging disqus

--- src/components/Disqus.js
+++ src/components/Disqus.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import DisqusThread from 'react-disqus-thread'
 
-const dev = process.env.NODE_ENV === 'development'
+const prod = window.location.hostname.match('reactnativeexpress.com')
+const shortname = prod ? 'reactnativeexpress' : 'reactnativeexpress-staging'
 
 export default class Disqus extends Component {
 
@@ -12,20 +13,9 @@ export default class Disqus extends Component {
   render() {
     const {identifier, title, url} = this.props
 
-    if (dev) {
-      return (
-        <DisqusThread
-          shortname={"reactnativeexpress"}
-          identifier={'development'}
-          title={'Development Title'}
-          url={'http://localhost:3210/development'}
-        />
-      )
-    }
-
     return (
       <DisqusThread
-        shortname={"reactnativeexpress"}
+        shortname={shortname}
         identifier={identifier}
         title={title}
         url={url}
