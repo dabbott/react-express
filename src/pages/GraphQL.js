caad09322ffa4830824ae92e4707c94d9c0692b6
Nikolas Burk <nikolas.burk@gmail.com>
2018-03-15 14:28:30 +0100

    update GraphQL pages

--- src/pages/GraphQL.js
+++ src/pages/GraphQL.js
@@ -16,9 +16,9 @@ const vendorComponents = [
 
 
 const content = markdown(markdownOptions)`
-[GraphQL](http://graphql.org/) is a *query language* for APIs. It enables declarative data fetching and thus ties in perfectly with React as a declarative framework for building user interfaces. GraphQL can either complement or entirely replace the usage of REST APIs. 
+[GraphQL](http://graphql.org/) is a *query language* for APIs. It enables declarative data fetching and thus ties in perfectly with React as a declarative framework for building user interfaces. GraphQL can either complement or entirely replace the usage of REST APIs.
 
-The main difference between REST and GraphQL is that RESTful APIs have *multiple endpoints* that return *fixed data structures* whereas a GraphQL server only exposes a *single endpoint* and returns *flexible data structures*. This works because a client that needs data from the server also submits its precise data requirements in each request which allows the server to tailor the response exactly according to the client’s needs. 
+The main difference between REST and GraphQL is that RESTful APIs have *multiple endpoints* that return *fixed data structures* whereas a GraphQL server only exposes a *single endpoint* and returns *flexible data structures*. This works because a client that needs data from the server also submits its precise data requirements in each request which allows the server to tailor the response exactly according to the client’s needs.
 
 You can learn more about the differences between GraphQL and REST [here](https://www.howtographql.com/basics/1-graphql-is-the-better-rest/). To get a high-level overview and understand more about the architectural use cases of GraphQL, take a look at [this](https://www.howtographql.com/basics/3-big-picture/) article.
 
@@ -28,6 +28,8 @@ GraphQL has a rapidly growing community. To stay up-to-date about everything tha
 
 * [GraphQL Weekly](https://www.graphqlweekly.com/): Weekly newsletter about GraphQL
 * [GraphQL Radio](https://www.graphqlradio.com/): Podcast discussing real-world use cases of GraphQL
+* [GraphQL Europe](https://www.graphql-europe.org/): Europe's biggest GraphQL conference
+* [Prisma blog](https://blog.graph.cool/): Technical deep dives and tutorials all around GraphQL development
 
 For an in-depth learning experience, visit the [How to GraphQL](https://www.howtographql.com/) fullstack tutorial website.
 `
