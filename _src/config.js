fbc524a4383e7f2b9177fce80574da47459cdaf2
Devin Abbott <devinabbott@gmail.com>
2020-06-21 12:19:04 -0700

    Remove unused files

--- _src/config.js
+++
@@ -1,5 +0,0 @@
-export const defaultTitle = "React Express";
-
-export const getTitle = title => {
-  return title ? `${title} - React Express` : defaultTitle;
-};
--- _src/examples/IndexRegisterApp.js
+++
@@ -1,6 +0,0 @@
-import { render } from "react-dom";
-import React from "react";
-
-import App from "./App";
-
-render(<App />, document.querySelector("#app"));
