86121aff596b4266c22db3a6e86cbbabff88bd3c
Devin Abbott <devinabbott@gmail.com>
2020-06-21 16:06:19 -0700

    More pages

---
+++ examples/files/react/unkeyedList.js
@@ -0,0 +1,13 @@
+const data = [
+  { id: 'a', name: 'Devin' },
+  { id: 'b', name: 'Gabe' },
+  { id: 'c', name: 'Kim' },
+]
+
+export default () => (
+  <div>
+    {data.map((item) => (
+      <div>{item.name}</div>
+    ))}
+  </div>
+)
