60709b65a6b035806c8d21d7f47f62b0e9d71559
Devin Abbott <devinabbott@gmail.com>
2020-06-21 16:41:08 -0700

    Improve JSX page

--- examples/files/jsx.js
+++ examples/files/jsx.js
@@ -1,5 +1,6 @@
 const element = (
-  <div foo="hello" bar={baz}>
-    <span>42</span>
+  <div>
+    <button />
+    <span />
   </div>
 )
---
+++ examples/files/jsxChildren.js
@@ -0,0 +1,9 @@
+const myVariable = 'Hello, world!'
+
+const element = (
+  <div>
+    Plain Text
+    <button />
+    {myVariable}
+  </div>
+)
