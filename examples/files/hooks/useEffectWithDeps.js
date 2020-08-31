fb668f70dab350ef3f328dfed033afb9e745135c
Devin Abbott <devinabbott@gmail.com>
2020-08-31 11:56:03 -0700

    Fix typos

--- examples/files/hooks/useEffectWithDeps.js
+++ examples/files/hooks/useEffectWithDeps.js
@@ -4,8 +4,8 @@ export default function App() {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
-    if (count > 0 && count % 5 == 0) {
-      alert(`${count} is divisible by 5!`)
+    if (count > 0 && count % 3 == 0) {
+      alert(`${count} is divisible by 3!`)
     }
   }, [count])
 
